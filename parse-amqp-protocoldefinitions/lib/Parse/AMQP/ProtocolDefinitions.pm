2b5d32f8b61137dcf4b0fc4f43fa18fb1a5214df
Pedro Melo <melo@simplicidade.org>
2009-10-13 16:59:17 +0100

    Parse basic metadata from top level node
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- parse-amqp-protocoldefinitions/lib/Parse/AMQP/ProtocolDefinitions.pm
+++ parse-amqp-protocoldefinitions/lib/Parse/AMQP/ProtocolDefinitions.pm
@@ -1,5 +1,54 @@
 package Parse::AMQP::ProtocolDefinitions;
 
 use Any::Moose;
+use XML::LibXML;
+
+has major => (
+  isa => 'Int',
+  is  => 'rw',
+);
+
+has minor => (
+  isa => 'Int',
+  is  => 'rw',
+);
+
+has revision => (
+  isa => 'Int',
+  is  => 'rw',
+);
+
+has port => (
+  isa => 'Int',
+  is  => 'rw',
+);
+
+no Any::Moose;
+__PACKAGE__->meta->make_immutable;
+
+###################################
+
+sub parse {
+  my ($self, $xml) = @_;
+
+  my $doc = XML::LibXML->load_xml(location => $xml);
+
+  $self->_extract_metadata($doc);
+}
+
+
+###################################
+
+sub _extract_metadata {
+  my ($self, $doc) = @_;
+
+  my ($amqp) = $doc->findnodes('/amqp');
+
+  for my $attr (qw( major minor revision port )) {
+    $self->$attr($amqp->getAttribute($attr));
+  }
+
+  return;
+}
 
 1;
---
+++ parse-amqp-protocoldefinitions/t/01-metadata.t
@@ -0,0 +1,20 @@
+#!perl
+
+use strict;
+use warnings;
+use Test::More;
+use Test::Exception;
+use Parse::AMQP::ProtocolDefinitions;
+
+plan skip_all => 'You need ENV AMQP_PROTO_DEFS_FILE'
+  unless $ENV{AMQP_PROTO_DEFS_FILE};
+
+my $pd = Parse::AMQP::ProtocolDefinitions->new;
+lives_ok sub { $pd->parse($ENV{AMQP_PROTO_DEFS_FILE}) };
+
+is($pd->major,    0);
+is($pd->minor,    9);
+is($pd->revision, 1);
+is($pd->port,     5672);
+
+done_testing();
