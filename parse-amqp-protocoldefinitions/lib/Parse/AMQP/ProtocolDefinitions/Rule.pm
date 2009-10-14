4ef556e2369a9666dc9e5351df8e13124657b72d
Pedro Melo <melo@simplicidade.org>
2009-10-14 15:28:03 +0100

    Add support for 'on-failure' attribute on Rule
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- parse-amqp-protocoldefinitions/lib/Parse/AMQP/ProtocolDefinitions/Rule.pm
+++ parse-amqp-protocoldefinitions/lib/Parse/AMQP/ProtocolDefinitions/Rule.pm
@@ -7,14 +7,18 @@ with
   'Parse::AMQP::ProtocolDefinitions::Roles::HasValidAttrs',
   'Parse::AMQP::ProtocolDefinitions::Roles::HasDocumentation';
 
+has on_failure => (
+  isa => 'Str',
+  is  => 'rw',
+);
+
 no Moose;
 __PACKAGE__->meta->make_immutable;
-
 
 ##############################
 
 sub xpath_expr  {'rule'}
-sub valid_attrs {qw()}
+sub valid_attrs {qw( on-failure )}
 
 ##############################
 
