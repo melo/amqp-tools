81b57f9291d1bc3efb9da586e43e51a6ca9b015f
Pedro Melo <melo@simplicidade.org>
2009-10-14 13:00:49 +0100

    Added another parsing role: ParseSequence
    Parses a sequence of elements, without any unique restrictions.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ parse-amqp-protocoldefinitions/lib/Parse/AMQP/ProtocolDefinitions/Roles/ParseSequence.pm
@@ -0,0 +1,24 @@
+package Parse::AMQP::ProtocolDefinitions::Roles::ParseSequence;
+
+use Moose::Role;
+
+requires(qw( xpath_expr ));
+
+sub parse {}
+
+sub parse_all {
+  my ($class, $doc) = @_;
+  my @all;
+  
+  for my $elem ($doc->findnodes($class->xpath_expr)) {
+    my $obj = $class->new;
+    $obj->parse($elem);
+    push @all, $obj;
+  }
+  
+  return \@all;
+}
+
+no Moose::Role;
+
+1;
