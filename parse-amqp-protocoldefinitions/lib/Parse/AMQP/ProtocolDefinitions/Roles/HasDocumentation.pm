736ffde096e30412354921dc2ec281d8ef3c2f33
Pedro Melo <melo@simplicidade.org>
2009-10-14 12:28:46 +0100

    HasDocumentation updated to support multiple docs
    Some elements have multiple <doc>'s, each one with a specific type
    attr.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- parse-amqp-protocoldefinitions/lib/Parse/AMQP/ProtocolDefinitions/Roles/HasDocumentation.pm
+++ parse-amqp-protocoldefinitions/lib/Parse/AMQP/ProtocolDefinitions/Roles/HasDocumentation.pm
@@ -2,25 +2,40 @@ package Parse::AMQP::ProtocolDefinitions::Roles::HasDocumentation;
 
 use Moose::Role;
 
-has doc => (
-  isa => 'Str',
-  is  => 'rw',
+has _docs => (
+  isa     => 'HashRef',
+  is      => 'rw',
+  default => sub { {} },
 );
+
+sub doc {
+  my ($self, $type) = @_;
+  my $docs = $self->_docs;
+
+  $type = '<empty>' unless $type;
+
+  return unless exists $docs->{$type};
+  return $docs->{$type};
+}
 
 sub type_name { return (split(/::/, shift))[-1] }
 
 around parse => sub {
   my $orig = shift;
   my ($class, $elem) = @_;
-  
+
   my $self = $orig->(@_);
-  
-  if (my $doc = $elem->getChildrenByTagName('doc')) {
-    my $value = $elem->textContent;
-    $self->doc($value) if defined $value;
+
+  my $docs = $self->_docs;
+  foreach my $doc ($elem->findnodes('doc')) {
+    my $type = $doc->getAttribute('type') || '<empty>';
+    my $value = $doc->textContent;
+    next unless $value;
+
+    $docs->{$type} = $value;
   }
   
-  return $self;
+  return $self
 };
 
 no Moose::Role;
