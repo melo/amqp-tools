b639e8c5ba6bfa789e98b12482ab3bb8410699fe
Pedro Melo <melo@simplicidade.org>
2009-10-19 16:02:38 +0100

    Make our 0.9.1 protocol API use the new register_version()
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- protocol-amqp/lib/Protocol/AMQP/V000009001.pm
+++ protocol-amqp/lib/Protocol/AMQP/V000009001.pm
@@ -1,5 +1,21 @@
 package Protocol::AMQP::V000009001;
 
+use Moose;
+use Protocol::AMQP::Registry;
+
 use Protocol::AMQP::V000009001::Connection;
+
+
+##################################
+
+Protocol::AMQP::Registry->register_version(
+  { major    => '0',
+    minor    => '9',
+    revision => '1',
+
+    api => 'Protocol::AMQP::V000009001',
+  },
+);
+
 
 1;
