4abbd4e91d0c5c1206671dd5e56e6f7207df97ce
Pedro Melo <melo@simplicidade.org>
2010-01-22 01:25:13 +0000

    Added proper responses from the client side to connect and tune
    We still need an API to let users tweak the password and auth mechanism,
    for now we are just hard-coding the OpenAMQ guest account.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- protocol-amqp/lib/Protocol/AMQP/Client.pm
+++ protocol-amqp/lib/Protocol/AMQP/Client.pm
@@ -1,6 +1,39 @@
 package Protocol::AMQP::Client;
+our $VERSION = '0.001';
 
 use Moose;
 extends 'Protocol::AMQP::Peer';
+
+##################################
+
+sub handle_method {
+  my ($self, $meth) = @_;
+  my $api  = $self->api;
+  my $name = $meth->{name};
+  my $data = $meth->{invocation};
+
+  if ($name eq 'connection_start') {
+    $api->connection->start_ok(
+      { client_properties => {
+          product => __PACKAGE__,
+          version => $VERSION,
+          contact => 'http://search.cpan.org/dist/Protocol-AMQP',
+        },
+        mechanism => 'PLAIN',
+        locale    => 'en-US',
+        response  => "\0guest\0guest",
+      }
+    );
+  }
+  elsif ($name eq 'connection_tune') {
+    $api->connection->tune_ok(
+      { channel_max => $data->{channel_max},
+        frame_max   => $data->{frame_max},
+        heartbeat   => $data->{heartbeat},
+      }
+    );
+  }
+}
+
 
 1;
